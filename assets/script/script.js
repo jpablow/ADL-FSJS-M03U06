ae1a341b292508760e87ee69f2bce6cde7f6d490
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-03-19 20:11:17 -0300

    Crea fx showFX y toCapitalLetter

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,29 @@
 const ctx = document.getElementById('myChart');
 const btn = document.querySelector("button");
+const fx_code = document.querySelector("#fx_code");
+const fx_clp = document.querySelector("#fx_clp");
 let fx = document.querySelector("#fx_options");
+// var dataDay = [];
 let grafico;
 let etiquetas = [];
 let datos = [];
+
+async function getDayFX() {
+    try {
+        const result = await fetch("https://mindicador.cl/api/");
+        const dataD = await result.json();
+        dataDay = dataD;
+    } catch (e) {
+        alert(e.message);
+    };
+};
 
 async function getFX() {
     try {
         const res = await fetch("https://mindicador.cl/api/" + fx.value);
         const data = await res.json();
         for (let i = 0; i < 10; i++) {
-            etiquetas[i] = data.serie[i].fecha;
+            etiquetas[i] = data.serie[i].fecha.substr(0, 10);
             datos[i] = data.serie[i].valor;
         };
         etiquetas.reverse();
@@ -21,11 +34,24 @@ async function getFX() {
     };
 };
 
+function toCapitalLetter (text) {
+    str = text.toString();
+    return str.charAt(0).toUpperCase() + str.substr(1, 30).toLowerCase();
+};
+
 function renderCanvas() {
     if (grafico) {
         grafico.destroy();
     };
-    let chartLabel = fx.value.toUpperCase();
+    switch (fx.value) {
+        case "uf":
+        case "utm":
+            var chartLabel = fx.value.toUpperCase();
+            break;
+        case "dolar":
+        case "euro":
+            var chartLabel = toCapitalLetter(fx.value);
+    };
     grafico = new Chart(ctx, {
         type: 'line',
         data: {
@@ -46,4 +72,26 @@ function renderCanvas() {
     });
 };
 
+function showFX() {
+    switch (fx.value) {
+        case "uf":
+            fx_code.innerHTML = fx.value.toUpperCase();
+            fx_clp.innerHTML = dataDay.uf.valor;
+            break;
+        case "dolar":
+            fx_code.innerHTML = toCapitalLetter(fx.value);
+            fx_clp.innerHTML = dataDay.dolar.valor;
+            break;
+        case "euro":
+            fx_code.innerHTML = toCapitalLetter(fx.value);
+            fx_clp.innerHTML = dataDay.euro.valor;
+            break;
+        case "utm":
+            fx_code.innerHTML = fx.value.toUpperCase();
+            fx_clp.innerHTML = dataDay.utm.valor;
+    };
+};
+
 btn.addEventListener("click", getFX);
+fx.addEventListener("change", showFX);
+window.addEventListener("load", getDayFX);
