6a0952ab386f94576e2b4ac78ebaaa23c3b0742f
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-03-19 18:15:32 -0300

    Crea script base

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,49 @@
-async function getTiposDeCambio() {
+const ctx = document.getElementById('myChart');
+const btn = document.querySelector("button");
+let fx = document.querySelector("#fx_options");
+let grafico;
+let etiquetas = [];
+let datos = [];
+
+async function getFX() {
     try {
-        const res =  await fetch("https://mindicador.cl/api/");
+        const res = await fetch("https://mindicador.cl/api/" + fx.value);
         const data = await res.json();
+        for (let i = 0; i < 10; i++) {
+            etiquetas[i] = data.serie[i].fecha;
+            datos[i] = data.serie[i].valor;
+        };
+        etiquetas.reverse();
+        datos.reverse();
+        renderCanvas();
     } catch (e) {
         alert(e.message);
-    }
-}
-getTiposDeCambio();
+    };
+};
+
+function renderCanvas() {
+    if (grafico) {
+        grafico.destroy();
+    };
+    let chartLabel = fx.value.toUpperCase();
+    grafico = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: etiquetas,
+            datasets: [{
+                label: 'Valor de: ' + chartLabel,
+                data: datos,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
+};
+
+btn.addEventListener("click", getFX);
